c38cce0aa9c3e91af1ba322ff51d8d08b33c03c4
Arthur <tuturley@gmail.com>
2025-05-15 09:33:51 -0300

    Comecei o segundo curso e finalizei a primeira Aula dele, Agora estou prestes a fazer os desafios

---
+++ Lógica de programação- explore funções e listas/Aulas/Aula 1/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do numero secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
+
+function verificarChute() {
+    console.log('O botão foi clicado')
+}
