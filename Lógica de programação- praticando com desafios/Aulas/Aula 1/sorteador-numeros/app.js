e521a541b7bce042bed31100366cdf378405ee5d
Arthur <tuturley@gmail.com>
2025-05-26 08:34:11 -0300

    Finalizado

--- Lógica de programação- praticando com desafios/Aulas/Aula 1/sorteador-numeros/app.js
+++ Lógica de programação- praticando com desafios/Aulas/Aula 1/sorteador-numeros/app.js
@@ -18,9 +18,30 @@ function sortear(){
 //Exibir os números aleatórios
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+    alterarStatusBotao();
 
 }
+
 //numero aleatorio
 function obterNumeroAleatorio(min, max){
     return Math.floor(Math.random() * (max-min + 1)) + min;
+}
+
+function alterarStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    if(botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    }else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
 }
