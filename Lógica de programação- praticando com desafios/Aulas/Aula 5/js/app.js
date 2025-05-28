f53491069cb8ad9344cec82b27922b8b189c79ca
Arthur <tuturley@gmail.com>
2025-05-28 16:20:17 -0300

    tentando fazer a aula 5 mas ta dificil, terminar amanhã

---
+++ Lógica de programação- praticando com desafios/Aulas/Aula 5/js/app.js
@@ -0,0 +1,27 @@
+let nomes = document.getElementById('nome-amigo').value;
+let espaço = ", "
+function adicionar(){
+//pegar o nome digitado
+let nomes = document.getElementById('nome-amigo').value;
+//atribuir a lista de amigos incluidos
+addNome = document.getElementById('lista-amigos').append(nomes, espaço)
+//limpar depois de colocar o nome
+document.getElementById('nome-amigo').innerHTML
+
+}
+
+function sortear(){
+//pegar os nomes da lista de amigos incluidos
+
+//embaralhar os nomes
+
+//atribuir a parte do hmtl "Sorteio"
+
+}
+
+function reiniciar(){
+//limpar a aba de Sorteio
+
+//limpar a lista de amigos incluidos
+
+}
