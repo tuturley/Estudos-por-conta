5fd6a4753e88fd10523a2db972ff693ee7512d34
Arthur <tuturley@gmail.com>
2025-06-11 09:09:42 -0300

    Realizei a primeira aula do curso novo e gostei de DOM

---
+++ cursos-JavaScript/JavaScript-manipulando-elementos-no-DOM/Desafios/script.js
@@ -0,0 +1,16 @@
+//Desafio 1:
+const html = document.querySelector('html');
+const timer = document.querySelector('#timer');
+const img = document.querySelector('.app__image');
+const text = document.querySelector('.app__title');
+//Desafio 2:
+console.log("html:", html, "timer:", timer, "img:", img, "text:", text);
+//Desafio 3:
+const startBtn = document.querySelector('.app__card-primary-button');
+const focoBtn = document.querySelector('.app__card-button--foco');
+const curtoBtn = document.querySelector('.app__card-button--curto');
+const longoBtn = document.querySelector('.app__card-button--longo');
+//Desafio 4:
+const focoTimer = 1500;
+const curtoTimer = 300;
+const longoTimer = 900;
