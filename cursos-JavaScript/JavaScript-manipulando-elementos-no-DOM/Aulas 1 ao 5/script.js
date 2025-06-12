5ab8871d921f43bcfcde77c5afcb16bef8f49cb0
Arthur <tuturley@gmail.com>
2025-06-12 09:13:35 -0300

    terminei aula 2 do curso de manipulação de DOM, na verdade falta fazer os desafios

--- cursos-JavaScript/JavaScript-manipulando-elementos-no-DOM/Aulas 1 ao 5/script.js
+++ cursos-JavaScript/JavaScript-manipulando-elementos-no-DOM/Aulas 1 ao 5/script.js
@@ -1,22 +1,46 @@
-const hmtl = document.querySelector('html');
+const html = document.querySelector('html');
 const focoBtn = document.querySelector('.app__card-button--foco');
 const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
-
+const titulo = document.querySelector('.app__title');
 
 
 focoBtn.addEventListener('click', () => {
-    hmtl.setAttribute('data-contexto', 'foco');
-    banner.setAttribute('src', './imagens/foco.png');
+alterarContexto('foco');
 });
 
 curtoBtn.addEventListener('click', () => {
-    hmtl.setAttribute('data-contexto', 'descanso-curto');
-    banner.setAttribute('src', './imagens/descanso-curto.png');
+alterarContexto('descanso-curto');
 });
 
 longoBtn.addEventListener('click', () => {
-    hmtl.setAttribute('data-contexto', 'descanso-longo')
-    banner.setAttribute('src', './imagens/descanso-longo.png');
+    alterarContexto('descanso-longo');
 });
+
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `./imagens/${contexto}.png`);
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+        
+        case "descanso-curto":
+            titulo.innerHTML = `
+            Que tal dar uma respirada?,<br>
+                <strong class="app__title-strong">Faça uma pausa curta.</strong>
+            `;
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `
+            Hora de voltar a superfície,<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>
+            `;
+        default:
+            break;
+    }
+}
