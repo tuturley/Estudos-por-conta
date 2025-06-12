e6d21b43885d3b41fe868216ba16c69f01ce9818
Arthur <tuturley@gmail.com>
2025-06-12 08:53:08 -0300

    add: Função de trocar imagens ao click adicionado

--- cursos-JavaScript/JavaScript-manipulando-elementos-no-DOM/Aulas 1 ao 5/script.js
+++ cursos-JavaScript/JavaScript-manipulando-elementos-no-DOM/Aulas 1 ao 5/script.js
@@ -2,15 +2,21 @@ const hmtl = document.querySelector('html');
 const focoBtn = document.querySelector('.app__card-button--foco');
 const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
+
+
 
 focoBtn.addEventListener('click', () => {
     hmtl.setAttribute('data-contexto', 'foco');
+    banner.setAttribute('src', './imagens/foco.png');
 });
 
 curtoBtn.addEventListener('click', () => {
     hmtl.setAttribute('data-contexto', 'descanso-curto');
+    banner.setAttribute('src', './imagens/descanso-curto.png');
 });
 
 longoBtn.addEventListener('click', () => {
-    hmtl.setAttribute('data-contexto', 'descanso-longo');
+    hmtl.setAttribute('data-contexto', 'descanso-longo')
+    banner.setAttribute('src', './imagens/descanso-longo.png');
 });
