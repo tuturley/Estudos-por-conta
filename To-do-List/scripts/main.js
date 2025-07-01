e9ace838a40f04de5ba8e5a151b3dead99196624
Arthur <tuturley@gmail.com>
2025-07-01 11:53:12 -0300

    atualização do Html, CSS, ambos finalizados, e inicio do JS

---
+++ To-do-List/scripts/main.js
@@ -0,0 +1,13 @@
+// DOM Elements: elementos do HTML
+const addTarefaBtn = document.getElementById('addTaskBtn');
+const limparTarefaBtn = document.getElementById('cleanTaskBtn');
+const feitoBtn = document.getElementsByClassName('completeTaskBtn');
+const naoFeitoBtn = document.getElementsByClassName('removeTaskBtn');
+const modoEscuro = document.getElementById('toggleTheme');
+// Variáveis: ex. lista de tarefas
+
+// Funções principais: adicionar, remover, completar, limpar
+
+// Eventos: onclick, onkeypress, etc
+
+// Inicialização: carregar do localStorage ao abrir
